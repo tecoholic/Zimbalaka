79de0adf67808b4beeab1eca2495f0ca88bd95f1
tecoholic <arun@arunmozhi.in>
2015-05-06 21:39:20 +0530

    filler post handler

--- zimbalaka.py
+++ zimbalaka.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 def index():
     if request.method == 'GET':
         return render_template('index.html')
+    if request.method == 'POST':
+        return 'Process started'
 
 if __name__ == "__main__":
     app.debug = True
